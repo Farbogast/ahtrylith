709e4561373be76c2275df106f4650525c1819c0
Farbogast <arbogast.francois@gmail.com>
2020-11-02 16:29:06 +0000

    DESKTOP VERSION

--- athrylith/src/component/Contact/Contact.js
+++ athrylith/src/component/Contact/Contact.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./Contact.css";
-import discord from "../../assets/img/discord-icon.png";
+import discord from "../../assets/img/discord1.png";
 import twitter from "../../assets/img/twitter.png";
-import twitch from "../../assets/img/twitch.png";
+import twitch from "../../assets/img/twitch1.png";
 import facebook from '../../assets/img/facebook.svg';
 import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
+import 'aos/dist/aos.css'; 
 AOS.init()
 
 
@@ -16,22 +15,22 @@ const Contact = () => {
       <h1 data-aos="fade-down" data-aos-duration="1000" id="h11">Nous Contacter</h1>
       <div className="contact">
         <div className="contact1" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
-          <a href="https://discord.gg/vy5WFg">
+          <a id="contact" href="https://discord.gg/vy5WFg" target='_blank' rel="noopener noreferrer"> 
             <img className="logo" src={discord} alt='discord'></img>
           </a>
         </div>
         <div className="contact1" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="800">
-          <a href="https://twitter.com/athrylithgaming">
+          <a id="contact" href="https://twitter.com/athrylithgaming" target='_blank' rel="noopener noreferrer">
             <img className="logo" src={twitter} alt='twitter'></img>
           </a>
         </div>
         <div className="contact1" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1100">
-          <a href="https://www.twitch.tv/athrylithgamingtv">
+          <a id="contact" href="https://www.twitch.tv/athrylithgamingtv" target='_blank' rel="noopener noreferrer">
             <img className="logo" src={twitch} alt='twitch'></img>
           </a>
         </div>
         <div className="contact1"data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1400">
-          <a href="https://www.facebook.com/AthrylithGaming">
+          <a id="contact" href="https://www.facebook.com/AthrylithGaming" target='_blank' rel="noopener noreferrer">
             <img className="logo" src={facebook} alt='facebook'></img>
           </a>
         </div>
--- athrylith/src/component/Roster/Roster.js
+++ athrylith/src/component/Roster/Roster.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Roster.css";
-import hearthstone from "../../assets/img/hearthstone.png"
+import hearthstone from "../../assets/img/hs.png"
 import smash from "../../assets/img/smash.png"
 import fifa from "../../assets/img/fifa.png"
 import AOS from 'aos';
@@ -8,27 +8,81 @@ import 'aos/dist/aos.css'; // You can also use <link> for styles
 // ..
 AOS.init();
 
-const Roster = () => {
+class Roster extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      isFlippedHS: false,
+      isFlippedSSBU:false,
+      isFlippedFIFA:false
+    }
+    this.handleClickHS = this.handleClickHS.bind(this)
+    this.handleClickSSBU = this.handleClickSSBU.bind(this)
+    this.handleClickFIFA = this.handleClickFIFA.bind(this)
+  }
+
+  handleClickHS(e) {
+    e.preventDefault();
+    this.setState(prevState => ({ isFlippedHS: !prevState.isFlippedHS }));
+  }
+
+  handleClickSSBU(e) {
+    e.preventDefault();
+    this.setState(prevState => ({ isFlippedSSBU: !prevState.isFlippedSSBU }));
+  }
+
+  handleClickFIFA(e) {
+    e.preventDefault();
+    this.setState(prevState => ({ isFlippedFIFA: !prevState.isFlippedFIFA }));
+  }
+ 
+  render() {
+    let isFlippedHS=this.state.isFlippedHS;
+    let isFlippedSSBU=this.state.isFlippedSSBU;
+    let isFlippedFIFA=this.state.isFlippedFIFA;
+
   return (
     <div className="roster">
       <h1 data-aos="fade-down" data-aos-duration="1000" id="h11">Nos équipes</h1>
       <div className="roster-container">
-        <div className="team-container"data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
+        <div onClick={this.handleClickHS} className="team-container"data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
         <h2 id="h22">Hearthstone</h2>
               <img src={hearthstone} alt="Conceptual" id="image11"></img>
         </div>
-        <div className="team-container" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="800">
+        {isFlippedHS && 
+          <div onClick={this.handleClickHS} className="player-container"data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
+        <h2 id="h22">Players:</h2>
+            <p>Diabblos(c)</p>
+            <p>Cocoaxe</p>
+            <p>Cariboukoko</p>
+            <p>LeCid</p>
+            <p>WinterCo</p>
+            <p>Trutru</p>
+        </div>}
+        <div onClick={this.handleClickSSBU} className="team-container" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="800">
         <h2 id="h22">Smash Bros Ultimate</h2>
               <img src={smash} alt="Conceptual" id="image22"></img>
         </div>
-        <div className="team-container" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1100">
+        {isFlippedSSBU && 
+          <div onClick={this.handleClickSSBU} className="player-container"data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
+        <h2 id="h22">Players:</h2>
+            <p>Mocra</p>
+            <p>Zora</p>
+        </div>}
+        <div onClick={this.handleClickFIFA} className="team-container" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1100">
         <h2 id="h22">FIFA 21</h2>
               <img src={fifa} alt="Conceptual" id="image33"></img>
         </div>
+        {isFlippedFIFA && 
+          <div onClick={this.handleClickFIFA} className="player-container"data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
+        <h2 id="h22">Players:</h2>
+            <p>Yanis</p>
+            <p>Cocoaxe</p>
+        </div>}
       </div>
   
     </div>
-  );
+  )};
 };
 
 export default Roster;
